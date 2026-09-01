ac614d849f0ded5e5469d9b2dddc4bdb7ffa4a6f
Dwyght <156776807+Dwyght@users.noreply.github.com>
2026-09-01 21:46:31 +0800

    feat: implement CORS filter to handle cross-origin requests

---
+++ src/main/java/com/svi/tictactoewebservice/filter/CorsFilter.java
@@ -0,0 +1,69 @@
+package com.svi.tictactoewebservice.filter;
+
+import java.io.IOException;
+
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@PreMatching
+public class CorsFilter
+        implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final String LOCALHOST_ORIGIN =
+            "http://localhost:5500";
+    private static final String LOOPBACK_ORIGIN =
+            "http://127.0.0.1:5500";
+
+    @Override
+    public void filter(ContainerRequestContext requestContext)
+            throws IOException {
+
+        String origin = requestContext.getHeaderString("Origin");
+
+        if (HttpMethod.OPTIONS.equals(requestContext.getMethod())
+                && isAllowedOrigin(origin)) {
+
+            requestContext.abortWith(
+                    Response.ok()
+                            .header("Access-Control-Allow-Origin", origin)
+                            .header(
+                                    "Access-Control-Allow-Methods",
+                                    "GET, POST, OPTIONS"
+                            )
+                            .header(
+                                    "Access-Control-Allow-Headers",
+                                    "Content-Type, Accept"
+                            )
+                            .build()
+            );
+        }
+    }
+
+    @Override
+    public void filter(
+            ContainerRequestContext requestContext,
+            ContainerResponseContext responseContext)
+            throws IOException {
+
+        String origin = requestContext.getHeaderString("Origin");
+
+        if (isAllowedOrigin(origin)) {
+            responseContext.getHeaders().putSingle(
+                    "Access-Control-Allow-Origin",
+                    origin
+            );
+        }
+    }
+
+    private boolean isAllowedOrigin(String origin) {
+        return LOCALHOST_ORIGIN.equals(origin)
+                || LOOPBACK_ORIGIN.equals(origin);
+    }
+}
